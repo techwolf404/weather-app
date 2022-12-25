61d53eec109f9eed9d4de99b4e8c6afbbfedfb4e
Codeaddictjaver <102610715+HatimDev-HE@users.noreply.github.com>
2022-12-25 18:07:35 +0300

    finished

--- weather.js
+++ weather.js
@@ -48,24 +48,26 @@ function parseCurrentWeather({ current_weather, daily }) {
 }
 
 function parseDailyWeather({ daily }) {
-    return daily.time.map((time, index) => {
-        return {
-            timestamp: time * 1000,
-            iconCode: daily.weathercode[index],
-            maxTemp: Math.round(daily.temperature_2m_max[index]), 
-        }
-    })
-} 
+  return daily.time.map((time, index) => {
+    return {
+      timestamp: time * 1000,
+      iconCode: daily.weathercode[index],
+      maxTemp: Math.round(daily.temperature_2m_max[index]),
+    }
+  })
+}
 
 function parseHourlyWeather({ hourly, current_weather }) {
-    return hourly.time.map((time, index) => {
-        return {
-            timestamp: time * 1000,
-            iconCode: hourly.weathercode[index],
-            temp: Math.round(hourly.temperature_2m[index]),
-            feelsLike: Math.round(hourly.apparent_temperature[index]),
-            windSpeed: Math.round(hourly.windspeed_10m[index]),
-            precip: Math.round(hourly.precipitation[index] * 100) / 100,
-        }
-    }).filter(({ timestamp }) => timestamp >= current_weather.time * 1000)
+  return hourly.time
+    .map((time, index) => {
+      return {
+        timestamp: time * 1000,
+        iconCode: hourly.weathercode[index],
+        temp: Math.round(hourly.temperature_2m[index]),
+        feelsLike: Math.round(hourly.apparent_temperature[index]),
+        windSpeed: Math.round(hourly.windspeed_10m[index]),
+        precip: Math.round(hourly.precipitation[index] * 100) / 100,
+      }
+    })
+    .filter(({ timestamp }) => timestamp >= current_weather.time * 1000)
 }
